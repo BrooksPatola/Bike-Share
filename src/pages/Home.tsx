def53b21e53ba296da4c1c4ebfbfc1ee004921f4
BrooksPatola <abpatola@myseneca.ca>
2020-03-30 22:24:59 -0400

    axios used to parse json data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent,
+   IonHeader, 
+   IonPage, 
+   IonTitle, 
+   IonToolbar, 
+   IonButton,
+   IonList,
+   IonItem,
+   useIonViewDidEnter,
+   useIonViewDidLeave,
+   useIonViewWillEnter,
+   useIonViewWillLeave } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import GoogleMapReact from 'google-map-react';
+import { useState } from 'react';
+
 
 const Home: React.FC = () => {
+
+  useIonViewDidEnter(() => {
+    console.log('ionViewDidEnter event fired');
+  });
+
+  useIonViewDidLeave(() => {
+    console.log('ionViewDidLeave event fired');
+  });
+
+  useIonViewWillEnter(() => {
+    console.log('ionViewWillEnter event fired');
+  });
+
+  useIonViewWillLeave(() => {
+    console.log('ionViewWillLeave event fired');
+  });
   return (
     <IonPage>
       <IonHeader>
@@ -12,10 +41,10 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent >
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">blank</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
--- src/pages/simpleMap.tsx
+++ src/pages/simpleMap.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
+import axios from 'axios';
 
 
 const SimpleMap = (props: any) => {
+  const sendGetRequest = () => {
+
+    return axios({
+      url: 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information',
+      method: 'get'
+    }).then(response => {
+  
+      console.log(response);
+      return response.data;
+    })
+  };
+
   const getMapOptions = (maps: any) => {
     return {
       disableDefaultUI: true,
@@ -13,9 +26,18 @@ const SimpleMap = (props: any) => {
     };
   };
 
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+
+    sendGetRequest().then(data => setItems(data.posts));
+
+  }, []);
+
     const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
     const [zoom, setZoom] = useState(11);
     return (
+ 
         <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyDgzdJamW7SttHqwhmPM5HH2mEtlKu-OEY' }}
